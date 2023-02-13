4b8cbe5ed24b222b3b9930c18c0a37751c941edc
Cairo Felipe <cairofelipedev@gmail.com>
2023-02-13 00:45:29 -0300

    Secrets

--- db_config.php
+++ db_config.php
@@ -1,8 +1,8 @@
 <?php 
 	$DB_HOST = 'localhost';
-	$DB_USER = '';
-	$DB_PASS = '';
-	$DB_NAME = '';
+	$DB_USER = '${{ secrets.DB_USER }}';
+	$DB_PASS = '${{ secrets.DB_PASS }}';
+	$DB_NAME = '${{ secrets.DB_NAME }}';
 	
 	try{
 		$DB_con = new PDO("mysql:host={$DB_HOST};dbname={$DB_NAME}",$DB_USER,$DB_PASS);
