b6872da9aa6d33048082a593089a6be88e426afc
Cairo <68676518+cairofelipedev@users.noreply.github.com>
2023-02-21 13:07:51 -0300

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,8 +1,7 @@
 <?php 
-	$DB_HOST = 'localhost';
-	$DB_USER = 'root';
-	$DB_PASS = 'root';
-	$DB_NAME = 'caixa';
+	$DB_USER = 'apcefp08_site';
+	$DB_PASS = '@Apcef321!02';
+	$DB_NAME = 'apcefp08_site';
 	
 	try{
 		$DB_con = new PDO("mysql:host={$DB_HOST};dbname={$DB_NAME}",$DB_USER,$DB_PASS);
